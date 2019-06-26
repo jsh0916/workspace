eb594e95be7ba3699d13625b5ceb27bb706d17b7
SooHyun <wjdtngus5511@naver.com>
2019-06-26 12:45:15 +0900

    ignore file adjust

---
+++ Problem01/src/SH/SecretMap.java
@@ -0,0 +1,14 @@
+package SH;
+
+public class SecretMap {
+	public static void main(String[] args) {
+		int number1 = 5;
+		int arr1[] = {9, 20, 28, 18, 11};
+		int arr2[] = {30, 1, 21, 17, 28};
+		boolean check = true;
+		
+		while (check) {
+			
+		}
+	}
+}
