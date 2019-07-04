75f0ec3f4a2e639038fd2d6029e299c8f9aedd8a
unknown <wjdtngus5511@naver.com>
2019-07-04 21:55:32 +0900

    IntQueue Tester add

--- Problem01/src/SH/IntQueue.java
+++ Problem01/src/SH/IntQueue.java
@@ -1,5 +1,7 @@
 package SH;
 
+import java.util.Scanner;
+
 // 링 버퍼로 큐 만들기
 public class IntQueue {
 	private int max;			// 큐의 용량
@@ -117,6 +119,58 @@ public class IntQueue {
 			}
 			
 			System.out.println();
+		}
+	}
+}
+
+class IntQueueTester {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		IntQueue s = new IntQueue(64);
+		
+		while (true) {
+			System.out.println("현재 데이터 수 : " + s.size() + " / " + s.capacity());
+			System.out.print("(1) 인큐	(2) 디큐		(3) 피크 		(4) 덤프		(0) 종료 : ");
+			
+			int menu = scan.nextInt();
+			if (menu == 0)
+				break;
+			
+			int x;
+			switch (menu) {
+				case 1:		// 인큐
+					System.out.print("데이터 : ");
+					x = scan.nextInt();
+					try {
+						s.enque(x);
+					} catch (IntQueue.OverflowIntQueueException e) {
+						System.out.println("큐가 가득찼습니다.");
+					}
+					
+					break;
+				case 2:		// 디큐
+					try {
+						x = s.deque();
+						System.out.println("디큐한 데이터는 " + x + " 입니다.");						
+					} catch (IntQueue.EmptyIntQueueException e) {
+						System.out.println("큐가 비어 있습니다.");
+					}
+					
+					break;
+				case 3:		// 피크
+					try {
+						x = s.peek();
+						System.out.println("peek 한 데이터는 " + x + " 입니다.");
+					} catch (IntQueue.EmptyIntQueueException e) {
+						System.out.println("큐가 비어 있습니다.");
+					}
+					
+					break;
+				case 4:		// 덤프
+					s.dump();
+					break;
+			}
 		}
 	}
 }
