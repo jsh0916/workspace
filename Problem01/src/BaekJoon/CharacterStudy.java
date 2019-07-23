148afb6a9a708247a84bb1c973fb68f2d45517b6
SH <wjdtngus5511@naver.com>
2019-07-23 16:50:44 +0900

    CharacterStudy modify

--- Problem01/src/BaekJoon/CharacterStudy.java
+++ Problem01/src/BaekJoon/CharacterStudy.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CharacterStudy {
 	public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
         String str = scan.nextLine().toUpperCase();
         boolean flag = true;								// 가장 많이 사용한 단어가 단수인지 복수인지 구분
         int[] mostUsedAlphabet = new int[26];				// 입력되는 문자열 모두 대문자로 변환
@@ -16,22 +16,21 @@ public class CharacterStudy {
         int max = 0;
         int maxIndex = 0;
         int duplicateMax = 0;
-
         for (int j = 0; j < mostUsedAlphabet.length; j++) {
         	if (max < mostUsedAlphabet[j]) {				// 가장 큰 값이 계속 업데이트 됨
         		max = mostUsedAlphabet[j];
         		maxIndex = j;
         	} else if (max == mostUsedAlphabet[j]) {
-        		duplicateMax = max;							// 최대값이 중복되는지 체크하기 위함
+        		duplicateMax = max;								// 최대값이 중복되는지 체크하기 위함
         	}
         }
-
+        
         if (duplicateMax == max) {
-        	flag = false;
+            flag = false;
         }
 
         if (flag) {
-        	System.out.println((char)(maxIndex + 65));				// 아스키 코드 변환
+        	System.out.println((char)(maxIndex + 65));		// 아스키 코드 변환
         } else {
         	System.out.println("?");
         }
