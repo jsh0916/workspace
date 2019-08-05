334fe7019a7d904fd4f9bc83b1e5f8d083034456
unknown <wjdtngus5511@naver.com>
2019-08-05 15:03:18 +0900

    9012 problem another solve

--- Problem01/src/BaekJoon/Baekjoon_9012.java
+++ Problem01/src/BaekJoon/Baekjoon_9012.java
@@ -3,8 +3,10 @@ package BaekJoon;
 import java.util.Scanner;
 import java.util.Stack;
 
+// https://www.acmicpc.net/problem/9012
 public class Baekjoon_9012 {
-	public static void main(String[] args) {
+	/*
+	 public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Stack stack = new Stack();
 		
@@ -36,4 +38,34 @@ public class Baekjoon_9012 {
 			stack.clear();
 		}
 	}
+	*/
+	
+	public static String isValid(String s) {
+        s = s.trim();
+        int n = s.length();
+        int cnt = 0;
+        for (int i=0; i<n; i++) {
+            if (s.charAt(i) == '(') {
+                cnt += 1;
+            } else {
+                cnt -= 1;
+            }
+            if (cnt < 0) {
+                return "NO";
+            }
+        }
+        if (cnt == 0) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        while (n-- > 0) {
+            System.out.println(isValid(sc.nextLine()));
+        }
+    }
 }
